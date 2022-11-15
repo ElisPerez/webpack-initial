fb82ed25ae4baff956dd3b8a863f7492180fa971
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-11-14 22:16:54 -0500

    file-loader para importar imagenes

--- src/js/components.js
+++ src/js/components.js
@@ -1,4 +1,5 @@
-import '../css/components.css'
+import '../css/components.css';
+import webpacklogo from '../assets/img/webpacklogo.png';
 
 export const greet = name => {
   console.log('Creando etiqueta h1');
@@ -7,4 +8,12 @@ export const greet = name => {
   h1.innerText = `Hello ${name}`;
 
   document.body.append(h1);
+
+  // Image
+  console.log(webpacklogo);
+  const img = document.createElement('img');
+  img.src = webpacklogo;
+  img.alt = 'Logo Webpack';
+
+  document.body.append(img);
 };
