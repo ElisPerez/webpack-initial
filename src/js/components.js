f0f9dc70d6b68c5e84e7571a97ce0c8c9085dd9b
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-11-16 12:49:15 -0500

    ahora si funciona. Era que tenia otra imagen en el component.js jeje

--- src/js/components.js
+++ src/js/components.js
@@ -1,5 +1,5 @@
 import '../css/components.css';
-import webpacklogo from '../assets/img/webpacklogo.png';
+// import webpacklogo from 'assets/img/webpacklogo.png';
 
 export const greet = name => {
   console.log('Creando etiqueta h1');
@@ -10,10 +10,10 @@ export const greet = name => {
   document.body.append(h1);
 
   // Image
-  console.log(webpacklogo);
-  const img = document.createElement('img');
-  img.src = webpacklogo;
-  img.alt = 'Logo Webpack';
-
-  document.body.append(img);
+  // console.log(webpacklogo);
+  // const img = document.createElement('img');
+  // img.src = webpacklogo;
+  // img.src = 'assets/img/webpack-logo.png';
+  // img.alt = 'Logo Webpack';
+  // document.body.append(img);
 };
